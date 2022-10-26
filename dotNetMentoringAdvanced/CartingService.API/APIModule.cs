ccab561c3cfb5ae2e0748543862ed967a7501192
Michal Surowiec <Michal_Surowiec@epam.com>
2022-10-26 21:47:56 +0200

    Add CartingService API

---
+++ dotNetMentoringAdvanced/CartingService.API/APIModule.cs
@@ -0,0 +1,17 @@
+﻿using CartingService.BusinessLayer;
+using CartingService.DataLayer;
+
+namespace CartingService.API
+{
+    internal static class APIModule
+    {
+        public static IServiceCollection RegisterDependencies(this IServiceCollection services)
+        {
+            services
+                .RegisterBusinessLayer()
+                .RegisterDataLayer();
+
+            return services;
+        }
+    }
+}
