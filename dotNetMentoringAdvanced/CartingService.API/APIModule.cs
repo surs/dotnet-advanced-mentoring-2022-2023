a9106341fb425fcc34404a2084cbea9921e05c94
Michal Surowiec <Michal_Surowiec@epam.com>
2022-10-29 11:48:43 +0200

    Add V2 controller

--- dotNetMentoringAdvanced/CartingService.API/APIModule.cs
+++ dotNetMentoringAdvanced/CartingService.API/APIModule.cs
@@ -1,5 +1,7 @@
 ﻿using CartingService.BusinessLayer;
 using CartingService.DataLayer;
+using V1 = CartingService.API.Controllers.V1;
+using V2 = CartingService.API.Controllers.V2;
 
 namespace CartingService.API
 {
@@ -9,7 +11,16 @@ namespace CartingService.API
         {
             services
                 .RegisterBusinessLayer()
-                .RegisterDataLayer();
+                .RegisterDataLayer()
+                .RegisterApiLayer();
+
+            return services;
+        }
+
+        public static IServiceCollection RegisterApiLayer(this IServiceCollection services)
+        {
+            services.AddSingleton<V1.CartController>();
+            services.AddSingleton<V2.CartController>();
 
             return services;
         }
---
+++ dotNetMentoringAdvanced/CartingService.API/Controllers/V2/CartController.cs
@@ -0,0 +1,38 @@
+﻿using CartingService.BusinessLayer.Entities;
+using CartingService.BusinessLayer.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CartingService.API.Controllers.V2
+{
+    [Route("v2/[controller]")]
+    public class CartController : Controller
+    {
+        private readonly ICartingService _cartingService;
+        private readonly V1.CartController _cartControllerV1;
+
+        public CartController(ICartingService cartingService, V1.CartController cartControllerV1)
+        {
+            _cartingService = cartingService;
+            _cartControllerV1 = cartControllerV1;
+        }
+
+        [HttpGet]
+        public IActionResult GetCart(Guid cartKey)
+        {
+            var result = _cartingService.GetCartItems(cartKey);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult AddItem(Guid cartKey, [FromBody] CartItem item)
+        {
+            return _cartControllerV1.AddItem(cartKey, item);
+        }
+
+        [HttpDelete]
+        public IActionResult RemoveItem(Guid cartKey, int itemId)
+        {
+            return _cartControllerV1.RemoveItem(cartKey, itemId);
+        }
+    }
+}
